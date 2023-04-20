15cf440c82ae39cda8a7554c516ab35f82fb60b0
Travis Nagle <NAGLET@ETSU.EDU>
2023-04-20 16:13:01 -0400

    Testing out some features

---
+++ CalculatorProject/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject
+{
+    internal class Calculator
+    {
+        public Dictionary<string, dynamic> dispatchTable = new();
+        public void Run()
+        {
+            dispatchTable["add"] = new Func<int, int, int>((x, y) => (x + y));
+            dispatchTable["sub"] = new Func<int, int, int>((x, y) => (x - y));
+            dispatchTable["mul"] = new Func<int, int, int>((x, y) => (x * y));
+            dispatchTable["div"] = new Func<int, int, int>((x, y) => (x / y));
+        }
+    }
+}
---
+++ CalculatorProject/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject
+{
+    internal class Menu
+    {
+        private Dictionary<int, string> menu = new();
+        private bool Quit { get; set; } = false;
+
+        public Menu()
+        {
+            Calculator calc = new Calculator();
+            Console.WriteLine("----------------CALCULATOR----------------");
+            Console.WriteLine("Please select an option");
+            Console.WriteLine("Start\tQuit");
+            string answer = Console.ReadLine();
+
+            if (answer == "Start")
+            {
+                while(!Quit)
+                {
+
+                }
+            }
+        }
+    }
+}
